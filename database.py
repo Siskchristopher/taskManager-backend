0e93ea6be0ad9b3a2fbc4ad27a98af953ab9525d
Siskchristopher <cws343@msstate.edu>
2026-04-22 16:38:59 -0500

    1.2 Database Infrastructure Setup

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Database file
+sqlalchemy_database_url = "sqlite:///./tasks.database.db"
+
+# Database connection (engine)
+engine = create_engine(sqlalchemy_database_url, connect_args={"check_same_thread": False})
+
+# Sessions 4 Database
+session_local = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+# Base Class
+base = declarative_base()
