bda441fd653e89704198aad351a15fdb3390e319
Siskchristopher <cws343@msstate.edu>
2026-04-23 09:33:45 -0500

    feat: 2.2 Implemented JWT Token Generation

--- auth.py
+++ auth.py
@@ -1,4 +1,19 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+
+secret_key = "super_secret_key"
+Algorithm = "HS256"
+access_token_expire_minutes = 30
+
+def create_access_token(data: dict):
+    # Copy of data
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=Algorithm)
+    return encoded_jwt
 
 # hashing engine setup
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
